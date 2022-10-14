288ba7ad42dbcc0e13ab04b43c0d925d024dbcea
durid-ah <ahmad.durid96@gmail.com>
2022-10-13 20:49:32 -0400

    dialog functionality

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,9 +1,25 @@
 import './style.css'
 // import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+// import { setupCounter } from './counter'
 
 const authDialog = document.getElementById('auth-dialog') as HTMLDialogElement;
 const launchAuthDialogButton = document.getElementById('open-auth-dlg');
+
+// AUTH INPUTS:
+const clientId = document.getElementById("auth-form-client-id") as HTMLInputElement;
+const clientSecret  = document.getElementById("auth-form-client-secret") as HTMLInputElement;
+const tenantId  = document.getElementById("auth-form-tenant-id") as HTMLInputElement;
+const subscriptionId = document.getElementById("auth-form-subscription-id") as HTMLInputElement;
+const submitButton = document.getElementById("auth-form-submit") as HTMLButtonElement;
+
+submitButton.addEventListener('click', () => {
+  submitButton.value = JSON.stringify({
+    clientId: clientId.value,
+    clientSecret: clientSecret.value,
+    tenantId: tenantId.value,
+    subscriptionId: subscriptionId.value
+  });
+});
 
 launchAuthDialogButton?.addEventListener('click', () => {
   authDialog.show();
