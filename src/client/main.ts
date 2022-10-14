4d8ab2391b8734bc7040bf97d373fa01ce0a0775
durid-ah <ahmad.durid96@gmail.com>
2022-10-13 21:03:20 -0400

    refactor auth dialog into its own file and move set auth into the navbar

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,33 +1,7 @@
 import './style.css'
+import './auth-dialog'
 // import typescriptLogo from './typescript.svg'
 // import { setupCounter } from './counter'
-
-const authDialog = document.getElementById('auth-dialog') as HTMLDialogElement;
-const launchAuthDialogButton = document.getElementById('open-auth-dlg');
-
-// AUTH INPUTS:
-const clientId = document.getElementById("auth-form-client-id") as HTMLInputElement;
-const clientSecret  = document.getElementById("auth-form-client-secret") as HTMLInputElement;
-const tenantId  = document.getElementById("auth-form-tenant-id") as HTMLInputElement;
-const subscriptionId = document.getElementById("auth-form-subscription-id") as HTMLInputElement;
-const submitButton = document.getElementById("auth-form-submit") as HTMLButtonElement;
-
-submitButton.addEventListener('click', () => {
-  submitButton.value = JSON.stringify({
-    clientId: clientId.value,
-    clientSecret: clientSecret.value,
-    tenantId: tenantId.value,
-    subscriptionId: subscriptionId.value
-  });
-});
-
-launchAuthDialogButton?.addEventListener('click', () => {
-  authDialog.show();
-});
-
-authDialog.addEventListener('close', () => {
-  console.log(`Dialog`, authDialog.returnValue);
-});
 
 
 // document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
