f40d5788ef1def108d27d4ba21eccd44586b54c1
durid-ah <ahmad.durid96@gmail.com>
2022-10-20 21:50:30 -0400

    test value

---
+++ frontend/src/task-models/test_val.ts
@@ -0,0 +1,31 @@
+import { AsyncResourceGroup, Resource, SequentialResourceGroup } from "./task_group.model";
+
+
+const TEST_TASKS = new SequentialResourceGroup([
+   new AsyncResourceGroup([
+      new Resource('Test Item 1')
+   ]),
+   new AsyncResourceGroup([
+      new Resource('Test Item 2'),
+      new Resource('Test Item 3')
+   ]),
+   new AsyncResourceGroup([
+      new Resource('Test Item 4'),
+      new Resource('Test Item 5'),
+      new Resource('Test Item 6')
+   ]),
+   new AsyncResourceGroup([
+      new SequentialResourceGroup([
+         new Resource('Test Item 7'),
+         new AsyncResourceGroup([
+            new Resource('Test Item 10'),
+            new Resource('Test Item 11'),
+            new Resource('Test Item 12')
+         ])
+      ]),
+      new Resource('Test Item 8'),
+      new Resource('Test Item 9')
+   ])
+])
+
+export default TEST_TASKS;
