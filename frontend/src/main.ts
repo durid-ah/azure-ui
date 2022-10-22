1dfb4820c3269f9b11990f0437557e003870fa96
durid-ah <ahmad.durid96@gmail.com>
2022-10-21 23:49:50 -0400

    dragstart event handling and setting the name data attribut

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,14 +4,24 @@ import TEST_TASKS from './task-models/test_val';
 import { AsyncResourceGroup, Resource, ResourceType } from './task-models';
 // import './socket_client'
 
+function card_dragstart_handler(ev: DragEvent) {
+   const targetEl = (ev.target as HTMLDivElement)
+   ev.dataTransfer?.setData('text/html', targetEl.outerHTML)
+   ev.dataTransfer!.dropEffect = "move";
+   console.log('DRAG: ', targetEl.dataset.name);
+}
+
 function createResourceCard(resource: Resource): HTMLDivElement {
    const cardElement = document.createElement('div');
    cardElement.classList.add('px-2');
    cardElement.setAttribute('draggable', 'true');
+   cardElement.dataset.name = resource.name;
    cardElement.innerHTML = `
       <div id="test-card-1" class="card card-compact bg-base-100 w-56 flex flex-row justify-center">
       </div>
    `;
+
+   cardElement.addEventListener('dragstart', card_dragstart_handler)
 
    const nameElement = document.createElement('div');
    nameElement.innerText = resource.name;
@@ -67,7 +77,6 @@ TEST_TASKS.tasks.forEach((item, idx) => {
    if (!isFirst) {
       el.classList.add('select-left');
    }
-
 });
 
 
