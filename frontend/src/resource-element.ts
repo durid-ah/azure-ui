70800d23919c25584bc9036079af145c3946ab54
durid-ah <ahmad.durid96@gmail.com>
2022-10-25 23:47:16 -0400

    cleanup the resource group logic

--- frontend/src/resource-element.ts
+++ frontend/src/resource-element.ts
@@ -2,6 +2,29 @@ import createResourceCard from "./card-element";
 import { AsyncResourceGroup, ResourceType } from "./task-models";
 import TEST_TASKS from "./task-models/test_val";
 
+function cleanUpResourceGroup(div: HTMLElement) {
+   const divId = div.id;
+   // TODO: Swap for state
+   // Get the group item's index in state
+   const idx = TEST_TASKS.tasks.findIndex(t => t.id.toString() === divId);
+   const lastIdx = TEST_TASKS.tasks.length - 1;
+
+   if (idx === 0) {
+      div.nextElementSibling?.remove();
+      div.nextElementSibling?.classList.remove('select-left')
+   } else {
+      div.previousElementSibling?.remove();
+   }
+
+   if (idx === lastIdx)
+      div.previousElementSibling?.classList.remove('select-right');
+
+   div.remove();
+   // TODO: Swap for state
+   TEST_TASKS.tasks = TEST_TASKS.tasks
+      .filter(t => t.id.toString() !== divId);
+}
+
 function dragOver_handler(ev: Event) {
    ev.preventDefault();
    (ev as DragEvent).dataTransfer!.dropEffect = "move";
@@ -15,44 +38,25 @@ function drop_handler(this: HTMLDivElement, ev: Event) {
    const movedFromGroup = droppedCard.parentElement!;
 
    if (this.id !== droppedCard?.parentElement?.id) {
-      // TODO: change to model counting?
+      // TODO: Update task in state
       this.appendChild(droppedCard);
    } 
 
    // If the group that the card was moved out of is empty remove group
-   const cardCount = movedFromGroup?.getElementsByClassName('card').length;
+   const cardCount = movedFromGroup?.getElementsByClassName('task-card').length;
    if (cardCount === 0) { 
-      const movedFromGroupId = movedFromGroup.id;
-      // TODO: Swap for state
-      const idx = TEST_TASKS.tasks.findIndex(t => t.id.toString() === movedFromGroupId);
-      const lastIdx = TEST_TASKS.tasks.length - 1;
-
-      if (idx === 0) {
-         movedFromGroup.nextElementSibling?.remove();
-         movedFromGroup.nextElementSibling?.classList.remove('select-left')
-      } else {
-         movedFromGroup.previousElementSibling?.remove();
-      }
-
-      if (idx === lastIdx)
-         movedFromGroup.previousElementSibling?.classList.remove('select-right');
-
-      movedFromGroup.remove();
-      // TODO: Swap for state
-      TEST_TASKS.tasks = TEST_TASKS.tasks
-         .filter(t => t.id.toString() !== movedFromGroupId);
+      cleanUpResourceGroup(movedFromGroup);
    }
+
+   const ghostCardEL = this.getElementsByClassName('ghost-card')[0];
+   this.appendChild(ghostCardEL);
 }
 
-function dragEnter_handler(this: HTMLDivElement, ev: Event) {
-   ev.preventDefault();
-   console.log('Entering', this.id);
-}
-
-function dropEnd_handler(ev: Event) {
-   const dropEv = ev as DragEvent;
-   dropEv.preventDefault();
-   dropEv.dataTransfer!.dropEffect = "move";
+function emptyCard() {
+   const el = document.createElement('div');
+   el.innerText = '+ Add Task';
+   el.classList.add('mx-2', 'card', 'ghost-card', 'w-56', 'text-center');
+   return el;
 }
 
 export default function createAsyncResourceGroup(resourceGroup: AsyncResourceGroup): HTMLDivElement {
@@ -60,10 +64,8 @@ export default function createAsyncResourceGroup(resourceGroup: AsyncResourceGro
    group.classList.add('task-group');
    group.id = resourceGroup.id.toString();
 
-   group.addEventListener("dragenter", dragEnter_handler);
    group.addEventListener("drop", drop_handler);
    group.addEventListener("dragover", dragOver_handler);
-   group.addEventListener("dropend", dropEnd_handler);
 
    resourceGroup.tasks.forEach(item => {
       if (item.itemType === ResourceType.Resource) {
@@ -71,6 +73,9 @@ export default function createAsyncResourceGroup(resourceGroup: AsyncResourceGro
          group.appendChild(el);
       }
    });
+
+   const emptyC = emptyCard();
+   group.appendChild(emptyC);
    
    return group;
 }
