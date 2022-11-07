fa1388ef87a37eddabd46f6e1fdc708325ec2a5a
durid-ah <ahmad.durid96@gmail.com>
2022-11-06 23:01:54 -0500

    wrap up work on the task element class

--- frontend/src/elements/task-element.ts
+++ frontend/src/elements/task-element.ts
@@ -2,26 +2,27 @@ import { Resource } from "../task-models";
 
 const cardClassNames = ['task-card', 'mx-2', 'card', 'card-compact', 'bg-base-100', 'w-56', 'flex', 'flex-row', 'justify-center', 'text-primary'];
 
-function dragStart_handler(ev: DragEvent) {
-   const targetEl = (ev.target as HTMLDivElement);
-   ev.dataTransfer?.setData('text/plain', targetEl.id)
-   ev.dataTransfer!.dropEffect = "move";
-}
-
+/**
+ * Represents a task on the board
+ */
 export class TaskElement {
    private taskData: Resource;
    public element: HTMLDivElement;
 
-   constructor(taskData: Resource, dragFunc = dragStart_handler) {
+   get id(): string {
+      return this.element.id;
+   }
+
+   constructor(taskData: Resource) {
       this.taskData = taskData;
       
       this.element = document.createElement('div');
       this.element.classList.add(...cardClassNames);
       this.element.setAttribute('draggable', 'true');
       this.element.dataset.name = taskData.name;
-      this.element.id = taskData.id;
+      this.element.id = this.taskData.id;
    
-      this.element.addEventListener('dragstart', dragFunc);
+      this.element.addEventListener('dragstart', (ev) => this.dragStart_handler(ev));
    
       const nameElement = document.createElement('div');
       nameElement.innerText = taskData.name;
@@ -29,5 +30,9 @@ export class TaskElement {
       this.element.appendChild(nameElement);  
    }
 
-
+   private dragStart_handler(ev: DragEvent) {
+      const targetEl = (ev.target as HTMLDivElement);
+      ev.dataTransfer?.setData('text/plain', targetEl.id)
+      ev.dataTransfer!.dropEffect = "move";
+   }
 }
